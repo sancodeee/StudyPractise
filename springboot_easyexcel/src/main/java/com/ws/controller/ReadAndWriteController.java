b22ac96d4250197c88bb2799b0e349b5ef3afea6
wangsen <1376474892@qq.com>
2022-12-30 15:10:08 +0800

    更新

--- springboot_easyexcel/src/main/java/com/ws/controller/ReadAndWriteController.java
+++ springboot_easyexcel/src/main/java/com/ws/controller/ReadAndWriteController.java
@@ -5,6 +5,7 @@ import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
 import com.ws.entity.Book;
 import com.ws.listener.BookListener;
+import com.ws.listener.BookListenerV2;
 import com.ws.result.ResponseResult;
 import com.ws.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-public class EasyExcelReadAndWrite {
+public class ReadAndWriteController {
 
     @Autowired
     private BookService bookService;
@@ -32,7 +33,7 @@ public class EasyExcelReadAndWrite {
                     .registerWriteHandler(new LongestMatchColumnWidthStyleStrategy())
                     .sheet(0,"书籍信息").doWrite(bookList);
 
-        return new ResponseResult(200,true,"下载成功!",bookList);
+        return new ResponseResult(200,true,"下载成功",bookList);
     }
 
     //读去excel接口
@@ -41,7 +42,14 @@ public class EasyExcelReadAndWrite {
         //基于监听器的读取实现
         EasyExcel.read(file.getInputStream(),Book.class,new BookListener()).sheet("书籍信息").headRowNumber(2).doRead();
 
-        return new ResponseResult(200,true,"读取成功！");
+        return new ResponseResult(200,true,"读取成功");
+    }
+
+    @PostMapping("/book/readv2")
+    public ResponseResult readExcelV2(@RequestPart("file") MultipartFile file) throws IOException{
+        //不创建实体对象的读取
+        EasyExcel.read(file.getInputStream(),new BookListenerV2()).sheet().headRowNumber(2).doRead();
+        return new ResponseResult(200,true,"读取成功");
     }
 
 
--- springboot_easyexcel/src/main/java/com/ws/listener/BookListener.java
+++ springboot_easyexcel/src/main/java/com/ws/listener/BookListener.java
@@ -3,13 +3,24 @@ package com.ws.listener;
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import com.alibaba.excel.metadata.CellData;
+import com.google.common.collect.Lists;
 import com.ws.entity.Book;
+import com.ws.service.BookService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 import java.util.Map;
-
+//创建对象的读取，通过实体对象读取
 @Slf4j
 public class BookListener extends AnalysisEventListener<Book> {
+
+    @Autowired
+    private BookService bookService;
+
+    //开辟一个长度为5的list空间当作缓存 防止一次性读取太多条数据内存溢出
+    private static final int BATCH_COUNT = 5;
+    private List<Book> cachedDataList = Lists.newArrayListWithExpectedSize(BATCH_COUNT);
 
     @Override
     public void invokeHead(Map<Integer, CellData> headMap, AnalysisContext context) {
@@ -20,6 +31,10 @@ public class BookListener extends AnalysisEventListener<Book> {
     public void invoke(Book book, AnalysisContext analysisContext) {
 
        log.info("读取一行数据：" + book);
+       cachedDataList.add(book);
+       if(cachedDataList.size() >= BATCH_COUNT){
+           cachedDataList = Lists.newArrayListWithExpectedSize(BATCH_COUNT);
+       }
 
     }
 
@@ -28,5 +43,16 @@ public class BookListener extends AnalysisEventListener<Book> {
 
         log.info("所有数据读取完毕！");
 
+    }
+
+    //数据持久化操作
+    public void savaData(){
+        //缓存满了往数据库存数据
+        boolean saveResult = bookService.saveBatch(cachedDataList);
+        if(saveResult == true){
+            log.info("数据持久化到数据库成功！");
+        }else {
+            log.error("数据持久化到数据库异常！");
+        }
     }
 }

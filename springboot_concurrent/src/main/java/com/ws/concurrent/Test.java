9ca0b329dc87f0715eb4581ac836dbd09479c8ae
wangsen <1376474892@qq.com>
2023-08-25 15:45:51 +0800

    gengxin

--- springboot_concurrent/src/main/java/com/ws/concurrent/Test.java
+++ springboot_concurrent/src/main/java/com/ws/concurrent/Test.java
@@ -1,23 +1,17 @@
 package com.ws.concurrent;
 
-
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class Test {
 
     @org.junit.jupiter.api.Test
-    public void test() throws ExecutionException, InterruptedException {
+    public void test() {
         //lambda表达式 通过继承Thread开启线程1
-        Thread threadOne = new Thread(() -> {
-            System.out.println("线程1-通过继承Thread类，重写run()方法实现");
-        });
+        Thread threadOne = new Thread(() -> System.out.println("线程1-通过继承Thread类，重写run()方法实现"));
 
         //lambda表达式 通过实现Runnable接口开启线程2
-        Runnable myRunnable = () -> {
-            System.out.println("线程2-通过实现Runable接口，实现run()方法开启线程");
-        };
+        Runnable myRunnable = () -> System.out.println("线程2-通过实现Runable接口，实现run()方法开启线程");
         Thread threadTwo = new Thread(myRunnable, "线程2");
 
         //lambda表达式 通过实现Callable接口开启线程3

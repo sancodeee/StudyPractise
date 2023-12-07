b4aee9481dc372e6d4f22862fda2513384b2215b
wangsen <1376474892@qq.com>
2023-12-07 10:41:56 +0800

    更新

--- springboot_algorithm/src/main/java/com/ws/nowcoder/HJ1.java
+++ springboot_algorithm/src/main/java/com/ws/nowcoder/HJ1.java
@@ -1,5 +1,8 @@
 package com.ws.nowcoder;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
 import java.util.Scanner;
 
 //字符串最后一个单词的长度
@@ -14,7 +17,7 @@ public class HJ1 {
     }
 
     //最后一个字符串长度
-    public static int LastStringLength(String str) {
+    public static int LastStringLength(@NotNull String str) {
         String[] split = str.split(" ");
         int i = split.length - 1;
         String s = split[i];

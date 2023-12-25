5fd2af953bcb9520acb5ba3f8f9bdb327a9b2c72
wangsen_a <wangsen_a@aspirecn.com>
2023-12-25 13:47:41 +0800

    排序算法

--- springboot_algorithm/src/main/java/com/ws/sort/demo.java
+++ springboot_algorithm/src/main/java/com/ws/sort/demo.java
@@ -3,17 +3,38 @@ package com.ws.sort;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Stack;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class demo {
 
     @Test
-    public void test() {
-        int[] arr = {4, 5, 5, 4, 5, 1};
-        // quickSort(arr, 0, arr.length - 1);
-        quickSort2(arr);
-        System.out.println(Arrays.toString(arr));
+    public void test() throws InterruptedException {
+        // int[] arr = {4, 5, 5, 4, 5, 1};
+        // // quickSort(arr, 0, arr.length - 1);
+        // quickSort2(arr);
+        // System.out.println(Arrays.toString(arr));
+
+        String str = "db.student.find({\"class\": {\"$in\":[\"1班\",\"3班\"]}})";
+        Pattern databaseNamePattern = Pattern.compile("db\\.([^.]+)\\.find");
+        Matcher matcher = databaseNamePattern.matcher(str);
+        if (matcher.find()) {
+            System.out.println("数据库名称：" + matcher.group(1));
+        }
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("1", "10");
+
+        final ReentrantLock reentrantLock = new ReentrantLock();
+        Condition condition = reentrantLock.newCondition();
+        condition.await();
+        condition.signal();
+        reentrantLock.lock();
+        reentrantLock.unlock();
     }
 
     public void quickSort(int[] arr, int low, int high) {
